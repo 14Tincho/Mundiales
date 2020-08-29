3b6f85a349aeac6af061d685ce8d6eb049e8e172
MartinCalozo <martincalozo@gmail.com>
2020-08-29 16:47:51 -0300

    empate de equipos 2

--- Mundiales/nos-clasificacion.php
+++ Mundiales/nos-clasificacion.php
@@ -3,6 +3,9 @@
 include 'conexion.php';
 $anio = ANO;
 
+$sql = "TRUNCATE TABLE clasificacion";
+$rs = mysqli_query($link, $sql);
+
 
 $sql = "SELECT id_equipo1, id_equipo2, goles1, goles2 FROM partidos WHERE anio = $anio AND fase = 'F';";
 
@@ -15,9 +18,11 @@ while ($partido = mysqli_fetch_assoc($rs)) :
     $idEquipo2 = $partido['id_equipo2'];
     $goles1 = $partido['goles1'];
     $goles2 = $partido['goles2'];
-    
+ if (!empty($goles1)){
+      
 //------------- empate-----------------
-    if ($goles1 == $goles2 ) {
+    if ($goles1 == $goles2) {
+// ------------Equipo1 empate----------
         $sql = "SELECT * FROM clasificacion WHERE id_equipo = $idEquipo1";
         $res = mysqli_query($link, $sql);
             if (mysqli_num_rows($res) !== 1 ) {
@@ -66,13 +71,62 @@ while ($partido = mysqli_fetch_assoc($rs)) :
                 WHERE id_equipo = $idEquipo1";
                 $resu = mysqli_query($link, $sql);
             }
+
+// ------------Equipo2 empate----------
+            $sql = "SELECT * FROM clasificacion WHERE id_equipo = $idEquipo2";
+            $res = mysqli_query($link, $sql);
+            if (mysqli_num_rows($res) !== 1 ) {
+            $sql = "INSERT INTO clasificacion (
+                id_equipo,
+                pj,
+                pg, 
+                pe, 
+                pp, 
+                gf, 
+                gc, 
+                dg, 
+                pts
+                ) VALUES (
+                $idEquipo2,
+                1,
+                0,
+                1,
+                0,
+                $goles2,
+                $goles1,
+                0,
+                1
+                )";
+            $resu = mysqli_query($link, $sql);
+            }else {
+                $updateclas = mysqli_fetch_assoc($res);
+                $pj  = $updateclas['pj'];
+                $pe  = $updateclas['pe']; 
+                $gf  = $updateclas['gf']; 
+                $gc  = $updateclas['gc']; 
+                $pts = $updateclas['pts'];
+
+                $pj++;
+                $pe++;
+                $gf += $goles2;
+                $gc += $goles1;
+                $pts ++;
+
+                $sql = "UPDATE clasificacion SET 
+                pj=$pj,
+                pe=$pe,
+                gf=$gf,
+                gc=$gc,
+                pts=$pts
+                WHERE id_equipo = $idEquipo2";
+                $resu = mysqli_query($link, $sql);
+            }
+            ?><pre><?php
+            var_dump($resu);
+            ?></pre><?php
     }
+}
 
     endwhile;
 
-
 mysqli_close($link);
-
-
-
-
