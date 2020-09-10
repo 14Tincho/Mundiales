3bfeaf911673d61d15043a43293bec4964c4f3e4
MartinCalozo <martincalozo@gmail.com>
2020-09-10 11:29:19 -0300

    Cree un eliminatorias nuevo para probar, dejando al que anda aparte

--- Mundiales/user-fixture-eliminatorias-div.php
+++ Mundiales/user-fixture-eliminatorias-div.php
@@ -1,59 +1,67 @@
-<div class="container text-center">
-    <form action="user-prode-fase.php" method="get">
-        <div class="form-row">
-            <h2 class="text-white col-12 titulo-octavos">Octavos</h2>
-            <?php 
-                $anio = ANO;
-                include 'conexion.php';
-                $sql = "SELECT 
-                o.nropartido,
-                o.posicion,
-                o.goles,
-                o.penales,
-                o.anio,
-                e.nombre,
-                e.grupo 
-                FROM octavos o 
-                LEFT JOIN equipos e ON e.id_equipo = o.id_equipo 
-                WHERE o.anio = $anio";
-                $rs = mysqli_query($link, $sql);
-                if (!$rs) {
-                    header('Location: pagina-error.php?error=2 & detalle=Error en la consulta');                    
-                    die;
-                }
-                mysqli_close($link);
-
-            while ($octavos = mysqli_fetch_assoc($rs)):
+<div class="form-row text-center">
+    <h2 class="text-white col-12 titulo-octavos">Octavos</h2>
+    <?php 
+        $anio = ANO;
+        include 'conexion.php';
+        $sql = "SELECT 
+        o.nropartido,
+        o.posicion,
+        o.goles,
+        o.penales,
+        o.anio,
+        e.nombre,
+        e.grupo 
+        FROM octavos o 
+        LEFT JOIN equipos e ON e.id_equipo = o.id_equipo 
+        WHERE o.anio = $anio";
+        $rs = mysqli_query($link, $sql);
+        if (!$rs) {
+            header('Location: pagina-error.php?error=2 & detalle=Error en la consulta');                    
+            die;
+        }
+        mysqli_close($link);
+            
+        $j = 1;
+        while ($octavos = mysqli_fetch_assoc($rs)):
             $nroPartido = $octavos['nropartido'];
             $posicion = $octavos['posicion'];
             $goles = $octavos['goles'];
             $penales = $octavos['penales'];
             $nombre = $octavos['nombre'];
             $grupo = $octavos['grupo'];
-            if (empty($goles)) {
-                $goles = '-';
-            }
-            if (empty($penales)) {
-                $penales = '-';
-            }?>
-            <?php if ($nroPartido <= 52) {
-            ?>
-                    <div class="form-group col-lg-3 d-block">
-                    <div>
-                        <h6 class="text-white">Partido <?= $nroPartido ?></h6>
-                    </div>
-                        <div class="form-row resultado-wrap ">
-                            <?php for ($i=0; $i < 2; $i++) { ?>
-                                    <div class="col-2 octavos"><span class="fixtureGoles"><?=$grupo . $posicion?></span></div>
-                                    <div class="col-6 div-bg"><?=$nombre?></div> 
-                                    <div class="col-2 octavos"><span class="fixtureGoles"><?=$goles?></span></div>
-                                    <div class="col-2 octavos"><span class="fixtureGoles">(<?=$penales?>)</span></div>
-                                    
-                            <?php } ?>
-                            </div>
-                    </div>
-                <?php } ?>
-            <?php endwhile; ?>
+        if (empty($goles)) {
+            $goles = '-';
+        }
+        if (empty($penales)) {
+            $penales = '-';
+        }
+        if ($nroPartido <= 52) {?>
+
+
+            <?php $j++;
+            ?><div class="form-group col-lg-3"><?php
+            if($j %2 == 0) { ?>
+            <h6 class="text-white">Partido <?= $nroPartido ?></h6>
+            <div class="form-row resultado-wrap ">
+                <div class="col-2 octavos"><span class="fixtureGoles"><?=$grupo . $posicion?></span></div>
+                <div class="col-6 div-bg"><?=$nombre?></div> 
+                <div class="col-2 octavos"><span class="fixtureGoles"><?=$goles?></span></div>
+                <div class="col-2 octavos"><span class="fixtureGoles">(<?=$penales?>)</span></div>
+            </div>
+
+            <?php
+            }else {?>
+            <div class="form-row resultado-wrap ">
+                <div class="col-2 octavos"><span class="fixtureGoles"><?=$grupo . $posicion?></span></div>
+                <div class="col-6 div-bg"><?=$nombre?></div> 
+                <div class="col-2 octavos"><span class="fixtureGoles"><?=$goles?></span></div>
+                <div class="col-2 octavos"><span class="fixtureGoles">(<?=$penales?>)</span></div>
+            </div>
+            <?php }
+            ?></div><?php
+        }?>
+                    
+        <?php endwhile; ?>
 
 
 
@@ -78,5 +86,4 @@
                     </div>
                 </div>           
                <?php } ?>
-    </form>
 </div>
